7cb6afd6584811497f572c56044fb2b56b66b4a4
kazemi-stock <mamad.kazemi1363@gmail.com>
2024-10-30 11:17:34 +0330

    defined CreditService.php arguments to the services.yaml

--- src/BasketCredit/Service/CreditService.php
+++ src/BasketCredit/Service/CreditService.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 
 namespace OxidEsales\ModuleTemplate\BasketCredit\Service;
 
-use OxidEsales\Eshop\Application\Model\Basket as EshopModelBasket;
+use OxidEsales\Eshop\Application\Model\Basket;
 use OxidEsales\Eshop\Core\Exception\VoucherException;
 use OxidEsales\ModuleTemplate\Voucher\Infrastructure\VoucherModelFactory;
 use OxidEsales\ModuleTemplate\Voucher\Infrastructure\VoucherSerieModelFactory;
@@ -17,19 +17,19 @@ use OxidEsales\ModuleTemplate\Voucher\Service\VoucherService;
 
 class CreditService implements CreditServiceInterface
 {
-    /** @var EshopModelBasket  */
-    private EshopModelBasket $oBasket;
+    /** @var Basket  */
+    private Basket $oBasket;
     private string $userId;
     private float $amount;
 
 
     /**
      * injection dependencies
-     * @param EshopModelBasket $oBasket
+     * @param Basket $oBasket
      * @param string $userId
      * @param float $amount
      */
-    public function __construct(EshopModelBasket $oBasket, string $userId, float $amount)
+    public function __construct(Basket $oBasket, string $userId, float $amount)
     {
         $this->oBasket = $oBasket;
         $this->userId = $userId;
